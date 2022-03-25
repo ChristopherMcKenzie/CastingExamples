d7aced852bef2f86d8f625b0757284ef15e2e3f6
ChristopherMcKenzie <christophermckenzie.1997@gmail.com>
2022-03-25 15:24:39 +0000

    First push to github

---
+++ CastingExample/CastingTests.cs
@@ -0,0 +1,98 @@
+﻿namespace CastingExample;
+
+using Xunit;
+public class CastingTests
+{
+    private static Mammal _mammal= new Mammal();
+    private Animal _animal = new Animal();
+    private Giraffe _giraffe = new Giraffe();
+    private Dog _dog = new Dog();
+    private SuperNova _superNova = new SuperNova();
+    
+    [Fact]
+    public void TestMammalCasting()
+    {
+        if (_mammal is Animal)
+        {
+            Assert.True(true);
+        }
+    }
+
+    [Fact]
+    public void TestAnimalCasting()
+    {
+        if (_animal is Mammal)
+        {
+            Assert.True(true);
+        }
+    }
+
+    [Fact]
+    public void TestGiraffeCasting()
+    {
+        if (_giraffe is Mammal)
+        {
+            if (_giraffe is Animal)
+            {
+                Assert.True(true);
+            }
+        }
+    }
+    [Fact]
+    public void TestDogCasting()
+    {
+        if (_dog is Mammal)
+        {
+            if (_dog is Animal)
+            {
+                Assert.True(true);
+            }
+        }
+    }
+    [Fact]
+    public void TestSuperNovaCasting()
+    {
+        if (_superNova is Animal)
+        {
+            Assert.True(true);
+        }
+        else
+        {
+            Assert.False(false);
+        }
+    }
+
+    [Fact]
+    public void TestListOfGiraffeCasting()
+    {
+        var listOfAnimal = new List<object>();
+        for (int i = 0; i < 5; i++)
+        {
+            listOfAnimal.Add(new Giraffe());
+        }
+        //cast the list of an animal to a generic animal
+        var animalList = listOfAnimal.Cast<Animal>().ToList();
+
+        if (animalList is List<Animal>)
+        {
+            Assert.True(true);
+        }
+    }
+    
+    //Using the as operator
+
+    [Fact]
+    public void TestGiraffeAsOperator()
+    {
+        var g = _giraffe as Mammal;
+        Assert.NotNull(g);
+        //Can't convert g from mammal to animal
+    }
+
+    [Fact]
+    public void TestDogAsOperator()
+    {
+        var g = _dog as Animal;
+        Assert.NotNull(g);
+    }
+}
--- CastingExample/Program.cs
+++ CastingExample/Program.cs
@@ -1,3 +1,97 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var g = new Giraffe();
+var d = new Dog();
+var a = new Animal();
+FeedMammals(g);
+FeedMammals(a);
+// Output:
+// Eating.
+// Animal is not a Mammal
 
-Console.WriteLine("Hello, World!");
+SuperNova sn = new SuperNova();
+TestForMammals(g);
+TestForMammals(d);
+TestForMammals(sn);
+TestForCastingTypes(g);
+TestForAnimalListCasting(g);
+TestForAnimalListCasting(d);
+
+//will throw exception
+TestNotAMammal(sn);
+
+static void FeedMammals(Animal a)
+{
+    if (a is Mammal m)
+    {
+        m.Eat();
+    }
+    else
+    {
+        // variable 'm' is not in scope here, and can't be used.
+        Console.WriteLine($"{a.GetType().Name} is not a Mammal");
+    }
+}
+
+static void TestForMammals(object o)
+{
+    // You also can use the as operator and test for null
+    // before referencing the variable.
+    var m = o as Mammal;
+    if (m != null)
+    {
+        Console.WriteLine(m.ToString());
+    }
+    else
+    {
+        Console.WriteLine($"{o.GetType().Name} is not a Mammal");
+    }
+}
+
+static void TestForCastingTypes(object o)
+{
+    var a = (Animal)o;
+    
+}
+static void TestForAnimalListCasting(object o)
+{
+    var listOfAnimal = new List<object>();
+    for (int i = 0; i < 5; i++)
+    {
+        listOfAnimal.Add(new Giraffe());
+        Console.WriteLine(listOfAnimal + " " + i);
+    }
+    //cast the list of an animal to a generic animal
+    var animalList = listOfAnimal.Cast<Animal>().ToList();
+
+    for (int i = 0; i < animalList.Count; i++)
+    {
+        if (animalList is List<Animal>)
+        {
+            Console.WriteLine("GiraffeList is not an animal list");
+        }
+    }
+}
+
+static void TestNotAMammal(object o)
+{
+    var fakeAnimal = o as Mammal;
+
+    Console.WriteLine(fakeAnimal.ToString());
+}
+// Output:
+// I am an animal.
+// SuperNova is not a Mammal
+
+public class Animal
+{
+    public void Eat() { Console.WriteLine("Eating."); }
+    public override string ToString()
+    {
+        return "I am an animal.";
+    }
+}
+
+public class Mammal : Animal { }
+public class Giraffe : Mammal { }
+public class Dog : Animal { }
+
+public class SuperNova { }
